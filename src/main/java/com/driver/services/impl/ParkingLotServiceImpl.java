b7c451d4228f2703a9e28094f10a88a5c371cccb
BHAVANI28122 <bhavanigavala@gmail.com>
2023-01-28 18:51:11 +0530

    do

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -73,7 +73,7 @@ public class ParkingLotServiceImpl implements ParkingLotService {
             Spot spot = new Spot();
             spot.setId(spotId);
             spot.setPricePerHour(pricePerHour);
-//            spot.setSpotType(SpotType.TWO_WHEELER);
+            spot.setSpotType(SpotType.TWO_WHEELER);
 //            spot.setOccupied(false);
             spot.setParkingLot(parkingLot);
             parkingLotRepository1.save(parkingLot);
